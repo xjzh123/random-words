091c22afb06431b69ac1436d14dbcef98b87eea9
xjzh123 <37945610+xjzh123@users.noreply.github.com>
2023-03-11 13:21:46 +0800

    update words; update css

--- script.js
+++ script.js
@@ -1,13 +1,16 @@
 function data() {
     let result = {}
-    result.word_list = words.split('\n')
-    result.len = result.word_list.length
-    result.choose = function (per_line, lines) {
+    result.all_words = words.split('\n')
+    result.len = result.all_words.length
+    result.getRandomWord = function () {
+        return result.all_words[Math.floor(Math.random() * result.len)]
+    }
+    result.getWords2d = function (per_line, lines) {
         let choices = []
         for (let i = 0; i < lines; i++) {
             let line = []
             for (let j = 0; j < per_line; j++) {
-                let word = result.word_list[Math.floor(Math.random() * result.len)]
+                let word = result.getRandomWord()
                 line.push(word)
             }
             line.id = Math.random()
@@ -15,12 +18,10 @@ function data() {
         }
         return choices
     }
-    if (localStorage['words']) {
-        result.chosen = JSON.parse(localStorage['words'])
-    } else {
-        localStorage['words'] = JSON.stringify(result.choose(4, 4))
-        result.chosen = JSON.parse(localStorage['words'])
+    if (!localStorage['words']) {
+        localStorage['words'] = JSON.stringify(result.getWords2d(4, 4))
     }
+    result.words2d = JSON.parse(localStorage['words'])
     return result
 }
 
