9667f237b1771dfac3841e4ef96461dcc8e5404b
xjzh123 <37945610+xjzh123@users.noreply.github.com>
2023-03-11 14:43:14 +0800

    middle commit

---
+++ merge.py
@@ -0,0 +1,24 @@
+import re
+
+with open('高考.txt', 'r', encoding='utf-8') as f:
+    gk = f.read().splitlines()
+
+with open('CET4.txt', 'r', encoding='utf-8') as f:
+    cet = f.read().splitlines()
+
+reg = re.compile(r""" |\(""")
+
+def get_word(line: str) -> str:
+    return reg.split(line)[0]
+
+cet_words = list(map(get_word, cet))
+
+result = cet.copy()
+
+for line in gk:
+    if get_word(line) not in cet_words or '(' in line or '&' in line:
+        result.append(line)
+
+result.sort()
+
+print('\n'.join(result), file=open('out2.txt', 'w', encoding='utf-8'))
